eebd32544a25efdc22fbf579453547eb900bb433
dan binnun <danbinnun1@gmail.com>
2020-09-09 11:11:27 +0300

    created ReaderServer.hpp

---
+++ src/ReaderServer.cpp
@@ -0,0 +1,33 @@
+#include "ReaderServer.hpp"
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <thread>
+#include <threads.h>
+#include <unistd.h>
+#include <memory>
+#include <cstring> 
+#include "ProblemInput.hpp"
+server_side::ReaderServer::ReaderServer(const std::string &end,
+                                        uint32_t maxClients,
+                                        std::chrono::milliseconds waitTime)
+    : m_end(end), m_maxClients(maxClients), m_waitTime(waitTime),
+      m_currentClients(0) {}
+
+void server_side::ReaderServer::serveClient(const int connfd,
+                                            const std::string &problem,
+                                            const std::string &alg) {
+  char buffer[1024]={0};
+  try{
+      read(connfd,buffer,sizeof(buffer));
+      std::unique_ptr<ProblemInput> input=std::make_unique<ProblemInput>(buffer);
+      bool clientFinished=false;
+      while (!clientFinished)
+      {
+          memset(buffer,0, sizeof(buffer));
+          std::thread readClientMessage(read,connfd,buffer,sizeof(buffer));
+          
+          //read(connfd,buffer, sizeof(buffer));
+      }
+      
+  }
+}
--- src/ReaderServer.hpp
+++ src/ReaderServer.hpp
@@ -8,15 +8,16 @@ namespace server_side
     {
     private:
         //what the client need to send to disconenect
-        std::string end;
+        std::string m_end;
         //maximum amount of clients the server can handle in the same time
-        uint32_t maxClients;
+        uint32_t m_maxClients;
         //amount of clients connected now
-        uint32_t currentClients;
+        uint32_t m_currentClients;
         //time to wait for the client to send
-        std::chrono::milliseconds waitTime;
+        std::chrono::milliseconds m_waitTime;
         void serveClient(const int connfd, const std::string& problem, const std::string& alg);
     public:
-        void open(int port) override;
+        void open(const int port) override;
+        ReaderServer(const std::string& end, uint32_t maxClients, std::chrono::milliseconds waitTime);
     };
 }
