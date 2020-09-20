8e06b7c057049d09f2508a57388e410b583e7e5d
Adi Efrati <adi.efrati@yahoo.com>
2020-09-20 03:00:00 +0300

    added a check to create the cache if it doesn't exist and to insert the solution into the cache in case it wasn't already in it in the solve method of FindGraphPath

--- src/FindGraphPath.cpp
+++ src/FindGraphPath.cpp
@@ -7,15 +7,22 @@
 #include "FindGraphPathInfo.hpp"
 #include "GraphPath.hpp"
 #include "ProblemException.hpp"
+#include <iostream>
 
 using namespace server_side;
 
 std::unique_ptr<Solution> FindGraphPath::solve() const {
+  if(!cacheExists()){
+    createCache();
+  }
   uint32_t hashCode = m_info.getHash();
   if (solutionInCache(hashCode)) {
     return std::make_unique<GraphPath>(searchByHash(hashCode));
+    std::cout<<"test"<<std::endl;
   }
-  return solveProblem();
+  std::unique_ptr<Solution> solution = solveProblem();
+  insert(hashCode, solution.get()->toString());
+  return solution;
 }
 
 FindGraphPath::FindGraphPath(const FindGraphPathInfo &info) : m_info(info) {}
