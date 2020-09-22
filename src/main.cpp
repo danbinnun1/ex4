fbe5fd2488685277fa337a69e97fbf2dfb78f467
dan binnun <danbinnun1@gmail.com>
2020-09-22 15:46:35 +0300

    added try and catch to server exception in main

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "ProblemSolver.hpp"
 #include "SerialServer.hpp"
 #include "Server.hpp"
+#include "ServerException.hpp"
 #include <iostream>
 #include <memory>
 #include <string.h>
@@ -19,32 +20,36 @@ int main(int argc, char *argv[]) {
   if (!server_side::cacheExists()) {
     server_side::createCache();
   }
-  if (argc > 3 || argc < 2) {
-    std::cerr << "wrong number of arguements" << std::endl;
-    return -1;
-  }
-  if (!is_int(argv[1])) {
-    std::cerr << "invalid port" << std::endl;
-    return -1;
-  }
-  int port = std::stol(argv[1]);
-  if (argc == 2) {
-    std::unique_ptr<server_side::Server> server =
-        std::make_unique<server_side::ParallelServer>();
-    server->open(port, std::make_unique<server_side::ProblemSolver>());
-  } else {
-    if (strcmp(argv[2], "serial") == 0) {
-      std::unique_ptr<server_side::Server> server =
-          std::make_unique<server_side::SerialServer>();
-      server->open(port, std::make_unique<server_side::ProblemSolver>());
-    } else if (strcmp(argv[2], "parallel") == 0) {
+  try {
+    if (argc > 3 || argc < 2) {
+      std::cerr << "wrong number of arguements" << std::endl;
+      return -1;
+    }
+    if (!is_int(argv[1])) {
+      std::cerr << "invalid port" << std::endl;
+      return -1;
+    }
+    int port = std::stol(argv[1]);
+    if (argc == 2) {
       std::unique_ptr<server_side::Server> server =
           std::make_unique<server_side::ParallelServer>();
       server->open(port, std::make_unique<server_side::ProblemSolver>());
     } else {
-      std::cerr << "invalid server type parameter" << std::endl;
-      return -1;
+      if (strcmp(argv[2], "serial") == 0) {
+        std::unique_ptr<server_side::Server> server =
+            std::make_unique<server_side::SerialServer>();
+        server->open(port, std::make_unique<server_side::ProblemSolver>());
+      } else if (strcmp(argv[2], "parallel") == 0) {
+        std::unique_ptr<server_side::Server> server =
+            std::make_unique<server_side::ParallelServer>();
+        server->open(port, std::make_unique<server_side::ProblemSolver>());
+      } else {
+        std::cerr << "invalid server type parameter" << std::endl;
+        return -1;
+      }
     }
+    return 0;
+  } catch (const server_side::ServerException &e) {
+    std::cerr << e.what() << std::endl;
   }
-  return 0;
 }
