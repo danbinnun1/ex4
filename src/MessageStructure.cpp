0f6b33ba64cbed702cc8731812338c28f003693a
dan binnun <danbinnun1@gmail.com>
2020-09-23 16:49:49 +0300

    fixed bug in message structure

--- src/MessageStructure.cpp
+++ src/MessageStructure.cpp
@@ -2,7 +2,7 @@
 
 std::string server_side::getStructure(const uint32_t status,
                                       const std::string &response) {
-  std::string s = "Version: 1.0\n";
+  std::string s = "Version: 1.0\r\n";
   s += "status: ";
   s += std::to_string(status);
   s += "\r\n";
