bcfbc9b26a5c6ca655c4348e9de15ebd11cf66dc
Adi Efrati <adi.efrati@yahoo.com>
2020-09-24 19:47:02 +0300

    added documentation in the cpp files of the algorithms

--- src/BFSProblem.cpp
+++ src/BFSProblem.cpp
@@ -9,26 +9,36 @@
 
 std::unique_ptr<server_side::Solution>
 server_side::BFSProblem::solveProblem() const {
+  //creates a matrix to keep track of the graph elements we already visited
   matrix::Matrix visited = matrix::Matrix(m_info.getMatrix().getHeight(),
                                           m_info.getMatrix().getWidth());
+  //creates a queue that holds the paths
   std::list<GraphPathService> queue;
+  //marks the starting graph element as visited and pushes it into the queue
   visited.setValue(m_info.getStartRow(), m_info.getStartCol(), 1);
   queue.push_back(GraphPathService());
 
+  //loops over the entire queue
   while (!queue.empty()) {
+    //removes the top path from the queue
     GraphPathService path = queue.front();
     queue.pop_front();
 
+    //gets the graph element that the path leads to
     GraphElement s = path.applyPath(m_info);
 
+    //checks if the path leads to the target element and returns it if it does
     if (s.getRow() == m_info.getEndRow() && s.getCol() == m_info.getEndCol()) {
       return std::make_unique<GraphPath>("BFS", path.getPath(),
                                          path.getPathWeight());
     }
+    //adds the variation of the path into the queue if it is possible and the path doesn't lead to a visited node
     if (s.getRow() != 0) {
       if (visited(s.getRow() - 1, s.getCol()) != 1) {
         double weight = m_info.getMatrix()(s.getRow() - 1, s.getCol());
+        //marks the graph element which the new path leads to as visited
         visited.setValue(s.getRow() - 1, s.getCol(), 1);
+        //checks if it is possible to pass through the graph element and adds it to the queue if it is
         if (weight != -1) {
           GraphPathService newPath(path);
           newPath.addDirection(Direction::up, weight);
@@ -39,7 +49,9 @@ server_side::BFSProblem::solveProblem() const {
     if (s.getCol() != 0) {
       if (visited(s.getRow(), s.getCol() - 1 != 1)) {
         double weight = m_info.getMatrix()(s.getRow(), s.getCol() - 1);
+        //marks the graph element which the new path leads to as visited
         visited.setValue(s.getRow(), s.getCol() - 1, 1);
+        //checks if it is possible to pass through the graph element and adds it to the queue if it is
         if (weight != -1) {
           GraphPathService newPath(path);
           newPath.addDirection(Direction::left, weight);
@@ -50,7 +62,9 @@ server_side::BFSProblem::solveProblem() const {
     if (s.getRow() != m_info.getMatrix().getHeight() - 1) {
       if (visited(s.getRow() + 1, s.getCol()) != 1) {
         double weight = m_info.getMatrix()(s.getRow() + 1, s.getCol());
+        //marks the graph element which the new path leads to as visited
         visited.setValue(s.getRow() + 1, s.getCol(), 1);
+        //checks if it is possible to pass through the graph element and adds it to the queue if it is
         if (weight != -1) {
           GraphPathService newPath(path);
           newPath.addDirection(Direction::down, weight);
@@ -61,7 +75,9 @@ server_side::BFSProblem::solveProblem() const {
     if (s.getCol() != m_info.getMatrix().getWidth() - 1) {
       if (visited(s.getRow(), s.getCol() + 1) != 1) {
         double weight = m_info.getMatrix()(s.getRow(), s.getCol() + 1);
+        //marks the graph element which the new path leads to as visited
         visited.setValue(s.getRow(), s.getCol() + 1, 1);
+        //checks if it is possible to pass through the graph element and adds it to the queue if it is
         if (weight != -1) {
           GraphPathService newPath(path);
           newPath.addDirection(Direction::right, weight);
--- src/BestFSProblem.cpp
+++ src/BestFSProblem.cpp
@@ -9,28 +9,38 @@
 
 std::unique_ptr<server_side::Solution>
 server_side::BestFSProblem::solveProblem() const {
+  //creates a matrix to keep track of the graph elements we already visited
   matrix::Matrix visited = matrix::Matrix(m_info.getMatrix().getHeight(),
                                           m_info.getMatrix().getWidth());
+  //creates a priority queue that holds the path which leads to the cheapest element in the front of it
   std::priority_queue<GraphPathService, std::vector<GraphPathService>,
                       LargerThanByLastWeight>
       queue;
+  //marks the starting graph element as visited and pushes it into the queue
   visited.setValue(m_info.getStartRow(), m_info.getStartCol(), 1);
   queue.push(GraphPathService());
 
+  //loops over the entire queue
   while (!queue.empty()) {
+    //removes the top path from the queue
     GraphPathService path = queue.top();
     queue.pop();
 
+    //gets the graph element that the path leads to
     GraphElement s = path.applyPath(m_info);
 
+    //checks if the path leads to the target element and returns it if it does
     if (s.getRow() == m_info.getEndRow() && s.getCol() == m_info.getEndCol()) {
       return std::make_unique<GraphPath>("BestFS", path.getPath(),
                                          path.getPathWeight());
     }
+    //adds the variation of the path into the queue if it is possible and the path doesn't lead to a visited node
     if (s.getRow() != 0) {
       if (visited(s.getRow() - 1, s.getCol()) != 1) {
         double weight = m_info.getMatrix()(s.getRow() - 1, s.getCol());
+        //marks the graph element which the new path leads to as visited
         visited.setValue(s.getRow() - 1, s.getCol(), 1);
+        //checks if it is possible to pass through the graph element and adds it to the queue if it is
         if (weight != -1) {
           GraphPathService newPath(path);
           newPath.addDirection(Direction::up, weight);
@@ -41,7 +51,9 @@ server_side::BestFSProblem::solveProblem() const {
     if (s.getCol() != 0) {
       if (visited(s.getRow(), s.getCol() - 1 != 1)) {
         double weight = m_info.getMatrix()(s.getRow(), s.getCol() - 1);
+        //marks the graph element which the new path leads to as visited
         visited.setValue(s.getRow(), s.getCol() - 1, 1);
+        //checks if it is possible to pass through the graph element and adds it to the queue if it is
         if (weight != -1) {
           GraphPathService newPath(path);
           newPath.addDirection(Direction::left, weight);
@@ -52,7 +64,9 @@ server_side::BestFSProblem::solveProblem() const {
     if (s.getRow() != m_info.getMatrix().getHeight() - 1) {
       if (visited(s.getRow() + 1, s.getCol()) != 1) {
         double weight = m_info.getMatrix()(s.getRow() + 1, s.getCol());
+        //marks the graph element which the new path leads to as visited
         visited.setValue(s.getRow() + 1, s.getCol(), 1);
+        //checks if it is possible to pass through the graph element and adds it to the queue if it is
         if (weight != -1) {
           GraphPathService newPath(path);
           newPath.addDirection(Direction::down, weight);
@@ -63,7 +77,9 @@ server_side::BestFSProblem::solveProblem() const {
     if (s.getCol() != m_info.getMatrix().getWidth() - 1) {
       if (visited(s.getRow(), s.getCol() + 1) != 1) {
         double weight = m_info.getMatrix()(s.getRow(), s.getCol() + 1);
+        //marks the graph element which the new path leads to as visited
         visited.setValue(s.getRow(), s.getCol() + 1, 1);
+        //checks if it is possible to pass through the graph element and adds it to the queue if it is
         if (weight != -1) {
           GraphPathService newPath(path);
           newPath.addDirection(Direction::right, weight);
